9fc10e82022846c208e05b76242d062fc2ce54af
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2025-04-03 12:25:15 +0300

    add project

---
+++ tests/workingWithAPI.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://conduit.bondaracademy.com/");
+});
+
+test("has title", async ({ page }) => {
+  await expect(page.locator(".navbar-brand")).toHaveText("conduit");
+});
+
+test.afterAll(async ({ page }) => {
+  await page.close();
+});
+
